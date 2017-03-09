c02b523700f0c4d329a7f1a61fc252df45cf7673
ruvor <rvoropaev@emdev.ru>
2017-03-09 16:18:29 +0700

    Ещё пара замечаний на будущее

--- ckeditor/plugins/tablestickyfilter/plugin.js
+++ ckeditor/plugins/tablestickyfilter/plugin.js
@@ -1,3 +1,5 @@
+//TODO: добавить JSDocов
+//TODO: переписать с использованием children, nextSiblingElement, table.rows, tr.cells, tr.rowIndex, td.cellIndex и т. п. (в sticky-filter, где не юзаются методы цкедитора)
 CKEDITOR.plugins.add( 'tablestickyfilter', {
     requires: 'tabletools,dialog,contextmenu',
     init: function( editor ) {
